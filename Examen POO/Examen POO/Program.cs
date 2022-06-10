917a1c05440ac4d4fd8471166a6dc897e612493c
Juan F Luna <105814988+JuanFLuna@users.noreply.github.com>
2022-06-10 11:42:55 -0700

    Add files via upload

---
+++ Examen POO/Examen POO/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Examen_POO
+{
+    class Program
+    {
+        class Inventario
+        {
+            //atributos 
+            public string Nombre;
+            public string Descripcion;
+            public float Precio;
+            public int Cantidad;
+
+            public Inventario(string Nombre, string Descripcion, float Precio, int Cantidad)
+            {
+                this.Nombre = Nombre;
+                this.Descripcion = Descripcion;
+                this.Precio = Precio;
+                this.Cantidad = Cantidad;
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+
+            try
+            {
+
+
+                StreamWriter prod = new StreamWriter("Producto.txt", true);
+
+                String str;
+
+                Console.WriteLine("Ingresando al sistema AMAZON");
+
+
+                Console.WriteLine("Escriba las líneas de texto para almacenar en el archivo.\n" + "Finalice cada línea pulsando la tecla<Entrar ");
+
+                // Lee una línea de la entrada estándar
+                str = Console.ReadLine();
+
+
+                Console.Write("Ingrese el nombre del producto: ");
+                string Nombre = Console.ReadLine();
+                Console.Write("Ingrese la descripcion del producto:  ");
+                string Descripcion = Console.ReadLine();
+                Console.Write("Ingrese el precio del producto: ");
+                float Precio = Single.Parse(Console.ReadLine());
+                Console.Write("Ingrese la cantidad que esta en Stock: ");
+                int Cantidad = int.Parse(Console.ReadLine());
+
+                Inventario e1 = new Inventario(Nombre, Descripcion, Precio, Cantidad);
+
+                prod.WriteLine(Nombre, Descripcion, Precio, Cantidad);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine("Ruta: " + e.StackTrace);
+            }
+            finally
+            {
+                // Cierra el archivo.
+                if (prod != null) prop.close();
+            }
+        }
+    }
+    
+}
